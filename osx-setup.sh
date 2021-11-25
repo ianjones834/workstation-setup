9631c6387d2af75acf74237d0c4bffcfc02276e4
ianjones834 <70075883+ianjones834@users.noreply.github.com>
2021-11-25 01:45:12 -0500

    ssh and pathway fixes

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chsh -s /bin/bash
-echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> .bash_profile
+echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bash_profile
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
@@ -9,12 +9,10 @@ brew install git
 git config --global user.name "Ian Jones"
 git config --global user.email ianjones834@protonmail.com
 
+mkdir ~/.shh
 touch ~/.ssh/config
 
-echo "Host *
-  AddKeysToAgent yes
-  UseKeychain yes
-  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
+printf "Host *\nAddKeysToAgent yes\nUseKeychain yes\nIdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
 
 ssh-keygen -t ed25519 -C ianjones834@protonmail.com
 eval "$(ssh-agent -s)"
@@ -22,6 +20,8 @@ ssh-add -K ~/.ssh/id_ed25519
 
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
+
+git clone git@github.com:ianjones834/workstation-setup.git ~/source
 
 source ~/.bash_profile
 
