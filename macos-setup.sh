ed48a72aa6f76827dabcec7263aee178d06d71cd
Ian Jones <ianjones834@protonmail.com>
2022-07-05 20:45:16 -0400

    update brews

--- macos-setup.sh
+++ macos-setup.sh
@@ -6,10 +6,9 @@ echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bash_profile
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
-brew updgrade
+brew upgrade
 
 brew install git
-brew install --cask docker
 
 git config --global user.name "Ian Jones"
 git config --global user.email "email"
@@ -23,18 +22,19 @@ ssh-keygen -t ed25519 -C "email"
 eval "$(ssh-agent -s)"
 ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
-
 #use pbcopy < ~/.ssh/id_ed25519.pub and add it to the SSH keys in your github account
 
 mkdir ~/source
 git clone git@github.com:ianjones834/workstation-setup.git ~/source
 
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+
 source ~/.bash_profile
 
-brew install --cask shiftit
-brew install --cask visual-studio-code
+brew install --cask rectangle
+brew install --cask docker
+brew install neovim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
